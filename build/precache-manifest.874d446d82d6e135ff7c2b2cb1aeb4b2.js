67547f6ec5745852fea35cba2f40cf2a75a405e6
Angelo Martins <angelo18@gmail.com>
2019-10-16 18:13:57 -0300

    manifest.json fix

---
+++ build/precache-manifest.874d446d82d6e135ff7c2b2cb1aeb4b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf59ab0352073aa1565628a7e00e18e",
+    "url": "/TabelaTurno/index.html"
+  },
+  {
+    "revision": "3faac81e7872d08ee646",
+    "url": "/TabelaTurno/static/css/main.156e5a3d.chunk.css"
+  },
+  {
+    "revision": "3219def14bb4da33b0af",
+    "url": "/TabelaTurno/static/js/2.4b76e57f.chunk.js"
+  },
+  {
+    "revision": "3faac81e7872d08ee646",
+    "url": "/TabelaTurno/static/js/main.6c28909f.chunk.js"
+  },
+  {
+    "revision": "5010cd7e3aad56747202",
+    "url": "/TabelaTurno/static/js/runtime-main.dac76f4a.js"
+  }
+]);
